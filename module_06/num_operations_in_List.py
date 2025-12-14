e9b2c11d4f9959db08431ed2893ad0a8022baeaa
parinitmishra9 <parinitmishra9@gmail.com>
2025-12-14 14:31:57 +0530

    Numerical operations on list elements

---
+++ module_06/num_operations_in_List.py
@@ -0,0 +1,19 @@
+numbers = [10, 4, -1, 20, 5.5, 7, 1]
+
+# Smallest number in the list
+# min()
+print(f"Smallest number: {min(numbers)}")
+
+# Biggest number in the list
+# max()
+print(f"Biggest number: {max(numbers)}")
+
+print("-----------------------")
+print()
+
+# Total of the numbers in the list
+# sum()
+print(f"Total: {sum(numbers)}")
+
+print("-----------------------")
+print()
