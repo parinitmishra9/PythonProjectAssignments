7651e91f32949986faab9315c38b5aea4fe0e00d
parinitmishra9 <parinitmishra9@gmail.com>
2025-12-14 13:24:00 +0530

    List introduction

---
+++ module_06/lists_introduction.py
@@ -0,0 +1,25 @@
+name = "John"
+age = 20
+percent = 85.5
+
+student = ["John", 20, 85.5]
+print(type(student))
+print(student)
+
+print("-----------------------")
+print()
+
+days_of_week = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+print(days_of_week)
+print(days_of_week[0])
+print(days_of_week[6])
+print(days_of_week[-1])
+print(days_of_week[-7])
+print(f"Last day of the week is {days_of_week[6]}")
+
+# Length of a list => the number of items/elements in the list
+print(len(days_of_week))
+print(f"Last day of the week is {days_of_week[-1]}")
+
+print("-----------------------")
+print()
