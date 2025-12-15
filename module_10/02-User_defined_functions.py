0ea003ef6124cbb9da97d597c280ed1f54cb0783
parinitmishra9 <parinitmishra9@gmail.com>
2025-12-15 23:38:36 +0530

    M10_02: User defined functions

---
+++ module_10/02-User_defined_functions.py
@@ -0,0 +1,11 @@
+# User defined functions
+# A user-defined function is a block of code that performs a specific task, defined by the user.
+# It is created using the def keyword followed by the function name and parentheses ().
+# The function can take parameters (inputs) and can return a value using the return statement.
+# Defining a user-defined function
+def add_numbers(a, b):
+    """This function adds two numbers and returns the result."""
+    print(f"The sum is: {a + b}")
+# Calling the user-defined function
+add_numbers(5, 3)
+# Output: The sum is: 8
